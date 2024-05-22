0f38a9b6f5165df9bc6a38622c6e0c8537bc0264
Luke Hashem <140243001+LukeHashem@users.noreply.github.com>
2024-05-22 11:50:11 +0100

    Uploading Edutest
    I am archiving the work I did as part of a team to develop Edutest.

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+// Hides strict standard output
+error_reporting(error_reporting() & ~E_STRICT & ~E_DEPRECATED); 
+
+require_once('jRoute/_load.php');
+
+$route = new jRoute("");
+
+//Pages
+
+$route->Route(['get'], '/', "pages/landing.php");
+
+$route->Route(['get'], '/leaderboard', "pages/leaderboard.php");
+
+$route->Route(['get'], '/login', "pages/login.php");
+
+$route->Route(['get'], '/login{error}', "pages/login.php");
+
+$route->Route(['get'], '/register', "pages/registration.php");
+
+$route->Route(['get'], '/studentDisplay', "pages/studentManagementDashboard.php");
+
+$route->Route(['get'], '/studentProfile/{id}', "pages/studentProfile.php");
+
+$route->Route(['get'], '/testDashboard', "pages/testDashboard.php");
+
+$route->Route(['get'], '/testManagement', "pages/testManagement.php");
+
+$route->Route(['get'], '/test_{resultID}', "pages/test.php");
+
+$route->Route(['get'], '/classManagement', "pages/classManagementTable.php");
+
+$route->Route(['get'], '/lecturerManagement', "pages/lecturerManagement.php");
+
+$route->Route(['get'], '/subjectManagement', "pages/subjectManagement.php");
+
+//Includes
+$route->Route(['get'], '/disableAccount/{id}', "functionality/disableAccount.php");
+
+$route->Route(['get'], '/approveAccount/{id}', "functionality/approveAccount.php");
+
+$route->Route(['get'], '/terminateAccount/{id}', "functionality/terminateAccount.php");
+
+echo $route->Dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
+
+?>
